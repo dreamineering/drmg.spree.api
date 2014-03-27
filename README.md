cf4fe2d8db08d9084e7d13882ef55304f7eedd09
Matt Mischewski <matt@dreamineering.com>
2014-03-27 23:19:59 +0100

    allow non updating routes to be accessed with out auth

---
+++ README.md
@@ -0,0 +1,10 @@
+
+
+# drmg.spree.api
+
+
+using spree with node and angular to provide an enhanced tablet experience
+
+See [stackmates](http://stackmates.dreamineering.com/stacks/core/dreamineering/) for the whys and hows
+
+
--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -9,6 +9,7 @@ Spree.config do |config|
   # Example:
   # Uncomment to override the default site name.
   # config.site_name = "Spree Demo Site"
+  Spree::Api::Config[:requires_authentication] = false
 end
 
 Spree.user_class = "Spree::User"
