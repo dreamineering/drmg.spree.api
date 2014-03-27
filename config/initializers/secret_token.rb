fee30df5aa36a6d03b5ab9e4aa69e33d0cc671fb
Matt Mischewski <matt@dreamineering.com>
2014-03-27 14:06:02 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EcomSpreeApi::Application.config.secret_key_base = '740155c7d11eeda2df40bbf2728aa2d08c90e7c22dd39b13ee7f2efe5ab868ef92d39ec176ca9f7da8902f5f928acc9af5fe5e202b68731a3bcb0eec5be9d612'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EcomSpreeApi::Application.config.session_store :cookie_store, key: '_ecom_spree_api_session'
